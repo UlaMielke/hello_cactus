731d0da0af9e21929e423d6674aa2ce6f41a1512
UMielke <mielkeurszula92@gmail.com>
2018-03-09 11:01:17 +0100

    Menu & Media Queries

---
+++ script.js
@@ -0,0 +1,8 @@
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width = '100vw';
+    document.getElementById('#section-a').style.marginLeft = '100vw';
+}
+function closeSlideMenu(){
+    document.getElementById('side-menu').style.width = '0px';
+    document.getElementById('#section-a').style.marginLeft = '0px';
+}
