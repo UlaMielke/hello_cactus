db96f3f7d1871f90cfc4a94c15ccace07ed1b676
UMielke <mielkeurszula92@gmail.com>
2018-03-13 12:01:09 +0100

    script edit

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function openSlideMenu(){
     document.getElementById('side-menu').style.width = '100vw';
-    document.getElementById('#section-a').style.marginLeft = '100vw';
 }
 function closeSlideMenu(){
     document.getElementById('side-menu').style.width = '0px';
-    document.getElementById('#section-a').style.marginLeft = '0px';
 }
